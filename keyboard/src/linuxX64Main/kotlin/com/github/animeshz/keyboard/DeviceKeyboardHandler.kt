e80205b4f614362001c0e7ac15aeb88b1aa6c4d6
Animeshz <animeshsahu19@yahoo.com>
2020-12-19 21:18:41 +0530

    Link to XLib and XInput dynamically.
    Lowering and Code Optimization on Linux.

---
+++ keyboard/src/linuxX64Main/kotlin/com/github/animeshz/keyboard/DeviceKeyboardHandler.kt
@@ -0,0 +1,42 @@
+package com.github.animeshz.keyboard
+
+import com.github.animeshz.keyboard.entity.Key
+import com.github.animeshz.keyboard.events.KeyEvent
+import com.github.animeshz.keyboard.events.KeyState
+import kotlinx.coroutines.flow.SharedFlow
+import platform.posix.geteuid
+
+@ExperimentalKeyIO
+@ExperimentalUnsignedTypes
+internal class DeviceKeyboardHandler : NativeKeyboardHandler {
+    override val events: SharedFlow<KeyEvent>
+        get() = TODO("Not yet implemented")
+
+    override fun sendEvent(keyEvent: KeyEvent, moreOnTheWay: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getKeyState(key: Key): KeyState {
+        TODO("Not yet implemented")
+    }
+
+    override fun isCapsLockOn(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isNumLockOn(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isScrollLockOn(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        internal fun create(): DeviceKeyboardHandler? {
+            if (geteuid() != 0U) return null
+
+            TODO()
+        }
+    }
+}
